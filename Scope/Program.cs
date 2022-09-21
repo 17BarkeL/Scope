7dc637e33c8ea84aa9a3bf502e099ae167cec979
Luke Barkess <luke.barkess@icloud.com>
2022-09-21 12:15:03 +0100

    hello

--- Scope/Program.cs
+++ Scope/Program.cs
@@ -10,6 +10,34 @@ namespace Scope
     {
         static void Main(string[] args)
         {
+            SayHello("English");
+            SayHello("French");
+            SayHello("German");
+            SayHello("yes");
+
+            Console.ReadLine();
+        }
+
+        static void SayHello(string language)
+        {
+            Console.WriteLine(language);
+
+            switch (language)
+            {
+                case "English":
+                    Console.WriteLine("Hello");
+                    break;
+                case "French":
+                    Console.WriteLine("Bonjour");
+                    break;
+                case "German":
+                    Console.WriteLine("Hallo");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that language");
+                    break;
+            }
+
         }
     }
 }
